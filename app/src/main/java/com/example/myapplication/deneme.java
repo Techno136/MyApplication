29a863dc3bcbeb482a1c60dc39a41340d7954ead
Techno136 <mustafakeskin136@hotmail.com>
2023-02-15 23:31:14 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/deneme.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+
+public class deneme {
+    public static void main(String[] args) {
+        merhaba("Mustafa");
+    }
+
+    // Metot Tanımlaması:
+    public static void merhaba(String isim) {
+        System.out.println("Merhaba " + isim + "! Hoş geldin.");
+    }
+}
